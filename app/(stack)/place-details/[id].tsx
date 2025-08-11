f4b3898cb4ff605be6e6b789bdfb88b94107c691
jvallejoromero <jvallejoromero@csustan.edu>
2025-08-11 15:16:08 -0700

    feat(place-details): ensure proper PlaceDetailsView version is rendered

--- app/(stack)/place-details/[id].tsx
+++ app/(stack)/place-details/[id].tsx
@@ -1,16 +1,27 @@
 import {useLocalSearchParams} from "expo-router";
 import LegacyPlaceDetailsView from "@/components/screens/LegacyPlaceDetailsView";
 import React from "react";
+import {useServices} from "@/context/ServicesContext";
+import {GoogleLegacyApiService} from "@/services/GoogleLegacyApiService";
+import {GoogleApiService} from "@/services/GoogleApiService";
+import PlaceDetailsView from "@/components/screens/PlaceDetailsView";
+import GenericErrorScreen from "@/components/screens/GenericErrorScreen";
 
 const PlaceDetails = () => {
+    const { googleApi } = useServices();
+
     const params = useLocalSearchParams<{ id: string }>();
     if (!params?.id) {
         return null;
     }
 
-    return (
-        <LegacyPlaceDetailsView id={params.id} />
-    );
+    if (googleApi instanceof GoogleLegacyApiService) {
+        return <LegacyPlaceDetailsView id={params.id} />;
+    } else if (googleApi instanceof GoogleApiService) {
+        return <PlaceDetailsView id={params.id} />;
+    } else {
+        return <GenericErrorScreen message={"Unsupported api version"} />;
+    }
 }
 
 export default PlaceDetails;
